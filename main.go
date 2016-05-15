343370d1acd3a6f43e123d7612ea98cceaa92b39
ivahaev <ivahaev@gmail.com>
2016-05-15 14:34:01 +0500

    echo bundled

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	_ "github.com/getblank/blank-sr/config"
+	"github.com/getblank/blank-sr/wango"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"golang.org/x/net/websocket"
 )
 
 func main() {
-
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		websocket.Handler(func(ws *websocket.Conn) {
+			wampServer.WampHandler(ws, nil)
+		})
+		return nil
+	})
+	wamp := wango.New()
+	e.Run(standard.New(":1323"))
 }
