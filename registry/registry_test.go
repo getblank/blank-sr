6720c77f09b7ef545752930cd5d8eff127137bc0
ivahaev <ivahaev@gmail.com>
2016-05-24 12:47:02 +0500

    tests fixed

--- registry/registry_test.go
+++ registry/registry_test.go
@@ -11,39 +11,44 @@ func TestRegister(t *testing.T) {
 
 	g.Describe("Service Register", func() {
 		g.Describe("#Register", func() {
+			g.Before(func() {
+				services = map[string][]Service{}
+			})
 			g.It("Should register service", func() {
-				s := Service{"type1", "addr1", "8888", "id1", ""}
-				register(s)
+				Register("type1", "addr1", "8888", "id1", "")
 				g.Assert(len(services["type1"])).Equal(1)
 			})
 
 			g.It("Should register same type service", func() {
-				s := Service{"type1", "addr2", "9999", "id2", ""}
-				register(s)
-				if len(services["type1"]) != 2 {
-					t.Fatal("Service was not registered")
-				}
+				Register("type1", "addr2", "9999", "id2", "")
+				g.Assert(len(services["type1"])).Equal(2)
 			})
 
 			g.It("Should register another type service", func() {
-				s := Service{"type3", "addr3", "8881", "id3", ""}
-				register(s)
+				Register("type3", "addr3", "8881", "id3", "")
 				g.Assert(len(services["type3"])).Equal(1)
 			})
 		})
 
 		g.Describe("#Unregister", func() {
+			g.Before(func() {
+				services = map[string][]Service{}
+				Register("type1", "addr1", "8888", "id1", "")
+				Register("type1", "addr2", "9999", "id2", "")
+				Register("type3", "addr3", "8881", "id3", "")
+			})
 			g.Describe("Should remove service", func() {
-				g.It("Should registered one service of type 'type1'", func() {
+				g.It("and in registry only one service of type 'type1'", func() {
+					lenBeforeRemoving := len(services["type1"])
 					Unregister("id1")
-					g.Assert(len(services["type1"])).Equal(1)
+					g.Assert(len(services["type1"])).Equal(lenBeforeRemoving - 1)
 				})
 				g.It("Last service must be with id 'id2'", func() {
 					g.Assert(services["type1"][0].connID).Equal("id2")
 				})
 			})
 			g.Describe("Should remove service of another type", func() {
-				g.It("Should registered zero service of type 'type2'", func() {
+				g.It("and registered zero service of type 'type2'", func() {
 					Unregister("id2")
 					g.Assert(len(services["type2"])).Equal(0)
 				})
